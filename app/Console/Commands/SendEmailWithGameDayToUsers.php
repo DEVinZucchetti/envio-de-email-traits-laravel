8aea3fbedffef7c8b92ab486f705feeeabe727cf
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2023-12-02 20:01:19 -0300

    feat: edita a descrição e a logica de execução da function handle

--- app/Console/Commands/SendEmailWithGameDayToUsers.php
+++ app/Console/Commands/SendEmailWithGameDayToUsers.php
@@ -2,7 +2,9 @@
 
 namespace App\Console\Commands;
 
+use App\Models\Product;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
 
 class SendEmailWithGameDayToUsers extends Command
 {
@@ -10,11 +12,16 @@ class SendEmailWithGameDayToUsers extends Command
     protected $signature = 'app:send-email-with-game-day-to-users';
 
 
-    protected $description = 'Command description';
+    protected $description = 'Envia um email com um pdf contendo o jogo do dia. Todos os dias as 12h';
 
 
     public function handle()
     {
 
+       $products = Product::query()->inRandomOrder()->take(1)->get();
+
+       foreach ($products as $product) {
+        Log::info($product->name);
+       }
     }
 }
