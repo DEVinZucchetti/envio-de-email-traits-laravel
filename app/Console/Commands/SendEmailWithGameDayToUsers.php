1a2a4a5d2951c66776f37f3839b55d502273470d
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2023-12-02 19:47:03 -0300

    feat: comando de envio de email para users

---
+++ app/Console/Commands/SendEmailWithGameDayToUsers.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class SendEmailWithGameDayToUsers extends Command
+{
+
+    protected $signature = 'app:send-email-with-game-day-to-users';
+
+
+    protected $description = 'Command description';
+
+
+    public function handle()
+    {
+
+    }
+}
