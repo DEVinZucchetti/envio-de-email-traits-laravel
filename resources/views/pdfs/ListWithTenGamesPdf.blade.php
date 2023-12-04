5c9c5ed5803f2cb9dbecc9de32796c830e45b3f1
Eliana Elizabeth Morillo Taco <eliana_taco@estudante.sesisenai.org.br>
2023-12-03 22:47:52 -0500

    feat:configura variables do pdf
    name, description, cover

--- resources/views/pdfs/ListWithTenGamesPdf.blade.php
+++ resources/views/pdfs/ListWithTenGamesPdf.blade.php
@@ -11,13 +11,15 @@
 
 <div style="text-align: center;">
     <h1>Lista de Games</h1>
+    <br>
+    <br>
 
     @foreach ($games as $game)
         <div style="border: 1px solid #ccc; padding: 10px; margin: 10px; display: inline-block; text-align: left;">
-            <img src="{{ $game['imagem'] }}" alt="{{ $game['nome'] }}" style="max-width: 100px; max-height: 100px;">
-            <h2>{{ $game['nome'] }}</h2>
-            <p><strong>Preço:</strong> R$ {{ $game['preco'] }}</p>
-            <p><strong>Descrição:</strong> {{ $game['descricao'] }}</p>
+            <img src="{{ $game['cover'] }}" alt="{{ $game['name'] }}" style="max-width: 100px; max-height: 100px;">
+            <h2>{{ $game['name'] }}</h2>
+            <p><strong>Preço:</strong> R$ {{ $game['price'] }}</p>
+            <p><strong>Descrição:</strong> {{ $game['description'] }}</p>
         </div>
     @endforeach
 </div>
